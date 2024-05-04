424b1ce461cfe0f8c568e668dadb6aa147b6a549
Ronazen <164463501+RonaZhai@users.noreply.github.com>
2024-05-04 15:55:52 +0800

    Update goiadmin.js

--- script/goiadmin.js
+++ script/goiadmin.js
@@ -16,15 +16,10 @@ module.exports.handleEvent = function({ api, event, admin }) {
 			if (event.mentions[id]) {
 				var msg = [
 					"Babe nalang iatawag mo sakanya",
-					"Stop mentioning my creator, he's busy 😗",
-					"My Creator is currently offline 😢",
-					"𝖠𝗇𝗈𝗍𝗁𝖾𝗋 𝗍𝖺𝗀 𝗂𝗇 𝗆𝗒 𝖺𝖽𝗆𝗂𝗇, 𝗂 𝗐𝗂𝗅𝗅 𝗉𝗎𝗇𝖼𝗁 𝗒𝗈𝗎 🙂",
-					"busy pa ata yun kaya mag-antay ka",
-					"Sorry, naka bebetime pa don't disturb him 🙄",
-					"Do you like my creator thats why your tagging him? Why dont you add him https://www.facebook.com/swordigo.swordslush 😏",
-					" Another tag in my Creator, i will kick your fucking ass"
+					"Stop mentioning my creator, she's busy 😗",
+					"My Creator is offline tulog kasi siya😭"
 				];
-				api.setMessageReaction("😍", event.messageID, (err) => {}, true);
+				api.setMessageReaction("🥺", event.messageID, (err) => {}, true);
 				return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
 			}
 		}
